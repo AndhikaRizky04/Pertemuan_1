d544b006d140e64df4c56ed932d118ea6fc47391
AndhikaRizky4 <dhika.10rpl.2019@gmail.com>
2022-09-05 21:50:29 +0700

    first commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Andhika Rizky Nur Wahyu" << endl;
+    cout << "A11.2022.14138" << endl;
+    cout << "Jl. Pemuda desa mijen Rt 05/04 Kec. Kaliwungu, Kab. Kudus" << endl;
+    cout << "SMK Raden Umar Said Kudus" << endl;
+    return 0;
+}
