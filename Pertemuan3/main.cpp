7d429c35a9133aac735e6366bf2511e2c4909d4d
AndhikaRizky04 <112884577+AndhikaRizky04@users.noreply.github.com>
2022-09-12 15:41:25 +0700

    Add files via upload

---
+++ Pertemuan3/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+    cout << "Andhika Rizky Nur Wahyu" << endl;
+    printf ("A11.2022.14138");
+    cout << endl;
+
+    int x = 18;
+    float t = 168.5;
+    char b = 'L';
+    string lb = "X-Large";
+
+    cout << "umur saya :" <<x;
+    cout << endl;
+    cout << "tinggi badan :" <<t;
+    cout << endl;
+    cout << "ukuran baju :" <<b;
+    cout << endl;
+    cout << "lebar baju :" <<lb;
+    cout << endl;
+    return 0;
+}
